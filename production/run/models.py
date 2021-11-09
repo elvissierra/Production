c997652a572585b56a92def146ace32920c18e19
elvis <elvissierra3@gmail.com>
2021-11-08 22:59:39 -0600

    set2

--- production/run/models.py
+++ production/run/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth import User
+from django.contrib.auth.models import User
 from django.db.models.fields.related import ManyToManyField
 
 
@@ -10,12 +10,12 @@ class Material(models.Model):
 
 class Function(models.Model):
     action = models.TextField(null=True, blank=True)
-    time = models.TimeField(auto_now=True, auto_now_add=True)
-    materials_used = models.ManyToManyField(Material, through="Cost")
+    time = models.TimeField(auto_now_add=True)
+    # materials_used = models.ManyToManyField(Material, through="Cost")
 
 
 class Area(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
     department = models.CharField(max_length=50)
-    item = models.ForeignKey(Material, on_delete=models.CASCADE)
+    material = models.ForeignKey(Material, on_delete=models.CASCADE)
     quantity = models.IntegerField()
