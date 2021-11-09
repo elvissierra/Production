3f89e2cf0ddc0f84ed9b2366184beb78b4354d6c
elvis <elvissierra3@gmail.com>
2021-11-09 00:07:25 -0600

    set2

--- production/run/admin.py
+++ production/run/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
+from django.db import models
 from .models import Material, Function, Area
 
-admin.site.register(Material, Function, Area)
+models = [
+    models.Material,
+    models.Function,
+    models.Area,
+]
+admin.site.register(models)
--- production/run/urls.py
+++ production/run/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from django.urls import include, path
 from django.contrib import admin
+from .views import ListView
 
 
 url_patterns = [
     path("admin/", admin.site.urls),
     path("", include("run.urls")),
+    path("department", ListView.as_view(), name="department"),
 ]
