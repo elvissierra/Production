ca1907fb2cf096e6ea7844ae97025f7e2edcc7eb
elvis <elvissierra3@gmail.com>
2021-11-09 23:54:10 -0600

    set3

--- production/run/admin.py
+++ production/run/admin.py
@@ -2,9 +2,6 @@ from django.contrib import admin
 from django.db import models
 from .models import Material, Function, Area
 
-models = [
-    models.Material,
-    models.Function,
-    models.Area,
-]
+models = [Material, Function, Area]
+
 admin.site.register(models)
