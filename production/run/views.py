03ff50c404c7f57a53f9b3e995fd977b1edab800
elvis <elvissierra3@gmail.com>
2021-11-11 21:31:52 -0600

    set5

--- production/run/views.py
+++ production/run/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from production.run.models import Area, Function, Material, Quality
+from production.run.models import Area, Function, Material, Members, Quality
 
 
 class Department(ListView):
@@ -9,15 +9,21 @@ class Department(ListView):
 
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
-        context["materials"] = context["materials"].filter(data=self.request.GET)
-        context["time"] = context["time"].add()
-        context["function"] = context["function"].
+        context["cost"] = context["price"].()
+        context["time"] = context["time"].()
+        context["members"] = Members.objects.all()
+        context["count"] = context["members"].count()
+        context["function"] = Function.objects.all()
+        context["count"] = context["function"].count()
+        context["materials"] = Material.objects.all()
+        context["count"] = context["materials"].count()
 
         return context
 
 
 class Tools(DetailView):
     model = Material
+    
 
 
 class ToolList(ListView):
