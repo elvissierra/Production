03f7bc260b3e8b1f6efb3a021c453b09e406adb6
elvis <elvissierra3@gmail.com>
2022-06-11 15:44:38 -0500

    .

--- run/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.db import models
-from .models import Person, Technicians, Processes, Tools, Department
-
-models = [Person, Technicians, Processes, Tools, Department]
-
-admin.site.register(models)
