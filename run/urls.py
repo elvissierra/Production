268eb1d035178e5ad51f1864a328b98b5321689e
elvis <elvissierra3@gmail.com>
2022-07-13 20:46:37 -0500

    working out views

--- run/urls.py
+++ run/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from . views import (BackendArea, CustomLoginView, PersonsPage, RegisterView, TechPage) 
+from .views import (CustomLoginView, RegisterView, Main) 
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
-    path('register/', RegisterView.as_view(), name = 'register_super'),
+    path('register_super/', RegisterView.as_view(), name = 'register_super'),
     path('logout/', LogoutView.as_view(next_page= 'login'), name= 'logout'),
-    path('', TechPage.as_view(), name= 'technicians')
+    path('', Main.as_view(), name = 'main'),
 ]
--- run/views.py
+++ run/views.py
@@ -1,14 +1,16 @@
 from msilib.schema import ListView
+from pyexpat import model
+from urllib import request
 from django.shortcuts import redirect, render
 from django.contrib.auth import login
+from django.views import View
 from django.views.generic.edit import FormView
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.forms import UserCreationForm
-from run.forms import PersonsForm
 from django.views.generic.list import ListView
+from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-from django.urls import reverse_lazy
 
 from .models import Person, Task, Technician, Tools, Area
 
@@ -26,7 +28,7 @@ class RegisterView(FormView):
     template_name = 'run/register_super.html'
     form_class = UserCreationForm
     redirect_authenticated_user = True
-    success_url = reverse_lazy('')
+    success_url = reverse_lazy('main')
 
     def form_valid(self, form):
         user = form.save()
@@ -36,37 +38,52 @@ class RegisterView(FormView):
 
     def get(self, *args, **kwargs):
         if self.request.user.is_authenticated:
-            return redirect('')#direct to main
+            return redirect('main')#direct to main
         return super(RegisterView, self).get(*args, **kwargs)
 
 
 #add new people
-class PersonsPage(LoginRequiredMixin):
+class RegisterPersons(LoginRequiredMixin, FormView):
     template_name = 'run/register_persons.html'
-    context_object_name = 'persons'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('register_persons')
 
-    def reg_person(request):
-        if request.method == 'POST':
-            form = PersonsForm(request.POST)
-            if form.is_valid():
-                try:
-                    form.save()
-                    return redirect('/main')
-                except:
-                    pass
-        else:
-            form = PersonsForm()
-        return render(request, '---', {'form':form})
+    def valid_register(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterPersons, self).valid_register(form)
+    
+"""
 
 #view all technicians-> info and what area they belong to
 class TechPage(ListView):
+    model = Technician
 
-"""
+    def show_techs(self, *args, **kwargs):
+        techs = Technician.objects.all()
+        return render(self, 'technicians.html', {'techs': techs})
+
+
 #display all tools and their info-> cost/ name/ areas their used/ description
 class ToolPage():
 
 #display each area- functions/ time/tools/technicians | add tools to area
 class AreaPage():
+"""    
+class ListView(View):
+    model = None
+    template_name = None
+    objects = None
+
+    def get(self, request, *args, **kwargs):
+        context = {'object_list': self.objects}
+        return render(request, self.template_name, context)
     
+
 #MAIN ATTRACTION | displays the cost for each function --> area --> dept total cost
-class BackendArea():"""
+class Main(ListView):
+    model = Technician
+    template_name = 'run/main.html'
+    objects = model.objects.all()
